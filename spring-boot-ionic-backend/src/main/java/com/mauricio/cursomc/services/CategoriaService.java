6227325bf68d2b98ff01df6f7d6ab58019c0dce5
Mauricio <mauricio.macario@palmsoft.com.br>
2023-06-26 18:14:55 -0300

    Disponibilizado serviço de deleção de categoria DELETE /categorias/{id}

--- spring-boot-ionic-backend/src/main/java/com/mauricio/cursomc/services/CategoriaService.java
+++ spring-boot-ionic-backend/src/main/java/com/mauricio/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.mauricio.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.mauricio.cursomc.domain.Categoria;
 import com.mauricio.cursomc.repositories.CategoriaRepository;
+import com.mauricio.cursomc.services.exceptions.DataIntegrityException;
 import com.mauricio.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -27,6 +29,16 @@ public class CategoriaService {
 	public Categoria update(Categoria categoria) {
 		find(categoria.getId());
 		return categoriaRepository.save(categoria);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			categoriaRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+		
 	}
 
 }
