b3d00e0703c2deed79cea08d1f24ca47fe5c953c
Mauricio <mauricio.macario@palmsoft.com.br>
2023-07-27 20:35:08 -0300

    Criando profile de teste

---
+++ spring-boot-ionic-backend/src/main/java/com/mauricio/cursomc/services/DBService.java
@@ -0,0 +1,158 @@
+package com.mauricio.cursomc.services;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mauricio.cursomc.domain.Categoria;
+import com.mauricio.cursomc.domain.Cidade;
+import com.mauricio.cursomc.domain.Cliente;
+import com.mauricio.cursomc.domain.Endereco;
+import com.mauricio.cursomc.domain.Estado;
+import com.mauricio.cursomc.domain.ItemPedido;
+import com.mauricio.cursomc.domain.Pagamento;
+import com.mauricio.cursomc.domain.PagamentoComBoleto;
+import com.mauricio.cursomc.domain.PagamentoComCartao;
+import com.mauricio.cursomc.domain.Pedido;
+import com.mauricio.cursomc.domain.Produto;
+import com.mauricio.cursomc.domain.enums.EstadoPagamento;
+import com.mauricio.cursomc.domain.enums.TipoCliente;
+import com.mauricio.cursomc.repositories.CategoriaRepository;
+import com.mauricio.cursomc.repositories.CidadeRepository;
+import com.mauricio.cursomc.repositories.ClienteRepository;
+import com.mauricio.cursomc.repositories.EnderecoRepository;
+import com.mauricio.cursomc.repositories.EstadoRepository;
+import com.mauricio.cursomc.repositories.ItemPedidoRepository;
+import com.mauricio.cursomc.repositories.PedidoRepository;
+import com.mauricio.cursomc.repositories.ProdutoRepository;
+
+@Service
+public class DBService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+
+	public void instantiateTestDatabase() {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
+		Categoria cat4 = new Categoria(null, "Eletrônicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
+
+		Produto p1 = new Produto(null, "Computador", 2000d);
+		Produto p2 = new Produto(null, "Impressora", 800d);
+		Produto p3 = new Produto(null, "Mouse", 80d);
+		Produto p4 = new Produto(null, "Mesa de Escritório", 300d);
+		Produto p5 = new Produto(null, "Toalha", 80d);
+		Produto p6 = new Produto(null, "Colcha", 80d);
+		Produto p7 = new Produto(null, "Tv true color", 80d);
+		Produto p8 = new Produto(null, "Roçadeira", 80d);
+		Produto p9 = new Produto(null, "Abajur", 80d);
+		Produto p10 = new Produto(null, "Pendente", 80d);
+		Produto p11 = new Produto(null, "Shampoo", 80d);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2, p4));
+		cat3.getProdutos().addAll(Arrays.asList(p5, p6));
+		cat4.getProdutos().addAll(Arrays.asList(p1, p2, p3, p7));
+		cat5.getProdutos().addAll(Arrays.asList(p8));
+		cat6.getProdutos().addAll(Arrays.asList(p9, p10));
+		cat7.getProdutos().addAll(Arrays.asList(p11));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1, cat4));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2, cat4));
+		p3.getCategorias().addAll(Arrays.asList(cat1, cat4));
+		p4.getCategorias().addAll(Arrays.asList(cat2));
+		p5.getCategorias().addAll(Arrays.asList(cat3));
+		p6.getCategorias().addAll(Arrays.asList(cat3));
+		p7.getCategorias().addAll(Arrays.asList(cat4));
+		p8.getCategorias().addAll(Arrays.asList(cat5));
+		p9.getCategorias().addAll(Arrays.asList(cat6));
+		p10.getCategorias().addAll(Arrays.asList(cat6));
+		p11.getCategorias().addAll(Arrays.asList(cat7));
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+		clienteRepository.save(cli1);
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
+		Pedido ped1 = new Pedido(null, LocalDateTime.of(2017, 9, 30, 10, 32), cli1, e1);
+		Pedido ped2 = new Pedido(null, LocalDateTime.of(2017, 10, 10, 19, 35), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, LocalDate.of(2017, 10, 20),
+				null);
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0d, 1, 2000d);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0d, 2, 80d);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100d, 1, 800d);
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+
+		p1.getItems().add(ip1);
+		p2.getItems().add(ip3);
+		p3.getItems().add(ip2);
+
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
+
+	}
+
+}
