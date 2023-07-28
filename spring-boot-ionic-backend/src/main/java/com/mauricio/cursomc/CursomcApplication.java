a293a7d73b22ef3d18f80d25c89ca8cff4e88be4
Mauricio <mauricio.macario@palmsoft.com.br>
2023-07-27 21:19:07 -0300

    Criando profile de desenvolvimento

--- spring-boot-ionic-backend/src/main/java/com/mauricio/cursomc/CursomcApplication.java
+++ spring-boot-ionic-backend/src/main/java/com/mauricio/cursomc/CursomcApplication.java
@@ -3,8 +3,10 @@ package com.mauricio.cursomc;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.mauricio.cursomc.domain"})
 public class CursomcApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
