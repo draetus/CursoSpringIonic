a68e7974d21effe60c4000afbbc560f9c134eb1d
Mauricio Farias <fariasjrmauricio@gmail.com>
2025-01-29 21:26:12 -0300

    Adicionado Refresh token

---
+++ spring-boot-ionic-backend/src/main/java/com/mauricio/cursomc/resources/AuthResource.java
@@ -0,0 +1,30 @@
+package com.mauricio.cursomc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mauricio.cursomc.security.JWTUtil;
+import com.mauricio.cursomc.security.UserSS;
+import com.mauricio.cursomc.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@PostMapping(value = "/refresh_token")
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", token);
+		return ResponseEntity.noContent().build();
+	}
+
+}
