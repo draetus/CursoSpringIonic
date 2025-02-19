c88c1c096b9cfcedca60b754ebe1039c28007708
Mauricio Farias <fariasjrmauricio@gmail.com>
2025-02-19 13:56:59 -0300

    Adicionado serviço para resgate de clientes por email

--- spring-boot-ionic-backend/src/main/java/com/mauricio/cursomc/resources/ClienteResource.java
+++ spring-boot-ionic-backend/src/main/java/com/mauricio/cursomc/resources/ClienteResource.java
@@ -41,6 +41,14 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@GetMapping(value = "/email")
+	public ResponseEntity<Cliente> find(@RequestParam(value = "value") String email) {
+		
+		Cliente obj = clienteService.findByEmail(email);
+		
+		return ResponseEntity.ok().body(obj);
+	}
+	
 	@PostMapping
 	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO clienteDTO) {
 		Cliente cliente = clienteService.fromDTO(clienteDTO);
--- spring-boot-ionic-backend/src/main/java/com/mauricio/cursomc/services/ClienteService.java
+++ spring-boot-ionic-backend/src/main/java/com/mauricio/cursomc/services/ClienteService.java
@@ -90,6 +90,19 @@ public class ClienteService {
 		return clienteRepository.findAll();
 	}
 	
+	public Cliente findByEmail(String email) {
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		Cliente obj = clienteRepository.findByEmail(email);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + user.getId() + ", Tipo: "+ Cliente.class.getName());
+		}
+		return obj;
+	}
+	
 	public Page<Cliente> findPage(Integer page, Integer size, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, size, Direction.valueOf(direction), orderBy);
 		return clienteRepository.findAll(pageRequest);
