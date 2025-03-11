ad62c5f2cd7935bb8088bff558f8f78294a7a52d
Mauricio Farias <fariasjrmauricio@gmail.com>
2025-03-11 16:18:05 -0300

    Adicionado projeto mongoDB workshop

---
+++ spring-boot-mongodb-workshop/src/main/java/com/mauricio/workshopmongo/SpringBootMongodbWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.mauricio.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongodbWorkshopApplication.class, args);
+	}
+
+}
