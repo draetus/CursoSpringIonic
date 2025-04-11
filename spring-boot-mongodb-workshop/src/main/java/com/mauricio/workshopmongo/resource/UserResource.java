80a15af6fb975bea45de8ae6b2532e91c1e25611
Mauricio Farias <fariasjrmauricio@gmail.com>
2025-04-10 21:02:20 -0300

    Obtendo usuario por id

--- spring-boot-mongodb-workshop/src/main/java/com/mauricio/workshopmongo/resource/UserResource.java
+++ spring-boot-mongodb-workshop/src/main/java/com/mauricio/workshopmongo/resource/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,11 +21,18 @@ public class UserResource {
 	@Autowired
 	private UserService userService;
 	
-	@GetMapping()
+	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> users = userService.findAll();
 		List<UserDTO> userDTOs = users.stream().map(UserDTO::new).toList();
 		return ResponseEntity.ok(userDTOs);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = userService.findById(id);
+		UserDTO userDTO = new UserDTO(user);
+		return ResponseEntity.ok(userDTO);
 	}
 	
 
--- spring-boot-mongodb-workshop/src/main/java/com/mauricio/workshopmongo/service/UserService.java
+++ spring-boot-mongodb-workshop/src/main/java/com/mauricio/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.mauricio.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mauricio.workshopmongo.domain.User;
 import com.mauricio.workshopmongo.repository.UserRepository;
+import com.mauricio.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,15 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		if (user.isEmpty()) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		
+		return user.get();
 	}
 
 }
