dc57170f28b02ba7eaf4d4e0fef0cc242cc15217
Mauricio Farias <fariasjrmauricio@gmail.com>
2025-05-08 20:05:48 -0300

    Consulta simples com query methods

--- spring-boot-mongodb-workshop/src/main/java/com/mauricio/workshopmongo/resource/PostResource.java
+++ spring-boot-mongodb-workshop/src/main/java/com/mauricio/workshopmongo/resource/PostResource.java
@@ -1,13 +1,17 @@
 package com.mauricio.workshopmongo.resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mauricio.workshopmongo.domain.Post;
+import com.mauricio.workshopmongo.resource.util.URL;
 import com.mauricio.workshopmongo.service.PostService;
 
 
@@ -22,6 +26,13 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post post = postService.findById(id);
 		return ResponseEntity.ok(post);
+	}
+	
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String title) {
+		title = URL.decodeParam(title);
+		List<Post> posts = postService.findByTitle(title);
+		return ResponseEntity.ok(posts);
 	}
 	
 
--- spring-boot-mongodb-workshop/src/main/java/com/mauricio/workshopmongo/service/PostService.java
+++ spring-boot-mongodb-workshop/src/main/java/com/mauricio/workshopmongo/service/PostService.java
@@ -1,5 +1,6 @@
 package com.mauricio.workshopmongo.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,10 @@ public class PostService {
 		}
 		
 		return post.get();
+	}
+	
+	public List<Post> findByTitle(String title) {
+		return postRepository.findByTitleContainingIgnoreCase(title);
 	}
 
 }
