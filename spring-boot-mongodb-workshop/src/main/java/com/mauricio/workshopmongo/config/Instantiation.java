c7d2047b5c1e5fe4f0a2574c65c3f1b68d8163a4
Mauricio Farias <fariasjrmauricio@gmail.com>
2025-05-08 18:48:28 -0300

    Projeção dos dados de author com DTO

--- spring-boot-mongodb-workshop/src/main/java/com/mauricio/workshopmongo/config/Instantiation.java
+++ spring-boot-mongodb-workshop/src/main/java/com/mauricio/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.mauricio.workshopmongo.domain.Post;
 import com.mauricio.workshopmongo.domain.User;
+import com.mauricio.workshopmongo.domain.dto.AuthorDTO;
 import com.mauricio.workshopmongo.repository.PostRepository;
 import com.mauricio.workshopmongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner{
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
-		Post post1 = new Post(null, LocalDate.of(2018, 3, 21), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, LocalDate.of(2018, 3, 23), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, LocalDate.of(2018, 3, 21), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, LocalDate.of(2018, 3, 23), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
